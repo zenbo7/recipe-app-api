ab03033a0b1a33179acebc119074b3e149c8c140
zenbo7 <matteocattaneo74@gmail.com>
2020-05-15 11:46:50 +0200

    Added tests, linted code, added superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -22,3 +23,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@zenbo7.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
