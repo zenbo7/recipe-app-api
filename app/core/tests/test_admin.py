240195fdc79a23b52a12598f436797e10fe42dfc
zenbo7 <matteocattaneo74@gmail.com>
2020-05-18 15:13:52 +0200

    added tests and configured django admin for listed users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,7 +17,7 @@ class AdminSiteTests(TestCase):
             password='test6789',
             name='Test user full name'
         )
-    
+
     def test_users_listed(self):
         """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
@@ -25,3 +25,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
